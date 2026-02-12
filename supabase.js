050eb6cabbd268a179fc9d3c07b0a0cfea89ccf0
shsussk <Francisperalta0416@gmail.com>
2026-02-12 18:14:43 -0400

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const SUPABASE_URL = 'https://rnlzbfadlujocggawxvl.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJubHpiZmFkbHVqb2NnZ2F3eHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MjgwMTAsImV4cCI6MjA4NjUwNDAxMH0.1ZxwST9yFSpDiPR7Iyd6WfRnozOzI9Ftms7dXiHRocE'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
